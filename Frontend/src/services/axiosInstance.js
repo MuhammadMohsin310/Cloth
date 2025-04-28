e549f008c0c55cdd4040301bc88dceb81a4a00cd
Fahad Siyal <fahadsiyal748@gmail.com>
2025-04-28 15:47:51 +0500

    Update axiosInstance.js

--- Frontend/src/services/axiosInstance.js
+++ Frontend/src/services/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'https://cloth-backend-pwni.vercel.app/api',
+    // baseURL: 'https://cloth-backend-pwni.vercel.app/api',
+    baseURL: 'http://localhost:3000/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
